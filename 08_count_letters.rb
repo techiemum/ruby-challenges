1b0e9c324da79ab4851ecd0fad50844dd629f7df
Asmaa Syed <techiemum@workmail.com>
2019-11-08 15:42:24 +1000

    asmaa

--- 08_count_letters.rb
+++ 08_count_letters.rb
@@ -10,8 +10,21 @@
 # Check your solution by running the tests:
 # ruby tests/08_count_letters_test.rb
 
-def count_letters (string)
-  result = {} # You'll need an empty hash to get started!
+  def count_letters (string)
+    result = {} # You'll need an empty hash to get started!
+    # hello = { "h" => "0", "e" => "1", "l" => "2", "l"=>"3", "o"=>4 }
+    # puts hello[""]
+      word_split = string.split("") 
+      word_split.each do |letter|
+      word_split.count(letter)
+     if !result.has_key?(letter)
+      result[letter] = word_split.count(letter)
+     end 
+    end 
+      return result
+  end 
 
-  return result # return the hash
-end
+
+
+p count_letters ("hello")
+p count_letters("mississippi")
